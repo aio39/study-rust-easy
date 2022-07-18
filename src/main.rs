0e3dd9b47c920f3ad57940472a6868bbd7952fe0
Aio <AioDev.JS@gmail.com>
2022-07-18 18:56:10 +0900

    15.Strings

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,20 @@
 fn main() {
-    print!("\t Start with a tab\nand move to a new line");
-    println!(
-        "Inside quotes
-you can write over
-many lines
-and it will print just fine."
+    let name = "서태지"; // $str is very fast
+    let other_name = String::from("Adrian Fahrenheit Țepeș"); // String is a bit slower, but more function / Heap / owned type / 24 bytes
+
+    // str is a dynamically sized type
+    // str은 가변 길이 이기때문에 pointer로 접근해야한다 (포인터는 사이즈가 같음.)
+
+    let a = "This is the string text".to_string();
+
+    let my_name = "Billybrobby";
+    let my_country = "USA";
+    let my_home = "Korea";
+
+    let together = format!(
+        "I am {} and I come from {} but I live in {}.",
+        my_name, my_country, my_home
     );
 
-    println!("He said, \"You can find the file at c:\\files\\my_documents\\file.txt.\" Then I found the file."); // We used \ five times here
-                                                                                                                 // r# is used to escape the string literal.
-    println!(
-        r#"He said, "You can find the file at c:\files\my_documents\file.txt." Then I found the file."#
-    );
-
-    let my_string = "'Ice to see you,' he said."; // single quotes
-    let quote_string = r#""Ice to see you," he said."#; // double quotes
-    let hashtag_string = r##"The hashtag #IceToSeeYou had become very popular."##; // Has one # so we need at least ##
-    let many_hashtags =
-        r####""You don't have to type ### to use a hashtag. You can just use #.""####; // Has three ### so we need at least ####
-
-    println!(
-        "{}\n{}\n{}\n{}\n",
-        my_string, quote_string, hashtag_string, many_hashtags
-    );
-
-    let r#let = 6; // The variable's name is let
-    let mut r#mut = 10; // This variable's name is mut
-
-    let my_number = r#return();
-    println!("{}", my_number);
-
-    println!("{:?}", b"This will look like numbers"); // b -> $str or chat to bo number
-
-    println!("{:X}", '행' as u32); // Cast char as u32 to get the hexadecimal value
-    println!("{:X}", 'H' as u32);
-    println!("{:X}", '居' as u32);
-    println!("{:X}", 'い' as u32);
-
-    println!("\u{D589}, \u{48}, \u{5C45}, \u{3044}"); // Try printing them with unicode escape \u
-
-    let number = 9;
-    let number_ref = &number;
-    println!("{:p}", number_ref);
-
-    let number = 555;
-    println!(
-        "Binary: {:b}, hexadecimal: {:x}, octal: {:o}",
-        number, number, number
-    );
-
-    let father_name = "Vlad";
-    let son_name = "Adrian Fahrenheit";
-    let family_name = "Țepeș";
-    println!(
-        "This is {1} {2}, son of {0} {2}.",
-        father_name, son_name, family_name
-    );
-
-    println!(
-        "{city1} is in {country} and {city2} is also in {country},
-but {city3} is not in {country}.",
-        city1 = "Seoul",
-        city2 = "Busan",
-        city3 = "Tokyo",
-        country = "Korea"
-    );
-
-    let title = "TODAY'S NEWS";
-    println!("{:-^30}", title); // no variable name, pad with -, put in centre, 30 characters long
-    let bar = "|";
-    println!("{: <15}{: >15}", bar, bar); // no variable name, pad with space, 15 characters each, one to the left, one to the right
-    let a = "SEOUL";
-    let b = "TOKYO";
-    println!("{city1:-<15}{city2:->15}", city1 = a, city2 = b); // variable names city1 and city2, pad with -, one to the left, one to the right
-}
-
-fn r#return() -> u8 {
-    // 함수도 r# 가능
-    println!("Here is your number.");
-    8
+    let my_string: String = "Try to make this a String".into(); // String::from()과는 다르게, into()는 변형 가능한 타입이 다양하므로 명시적으로 적어줘야한다.
 }
