06214103149ca2c68377cd6977d4c30909f6e378
Aio <AioDev.JS@gmail.com>
2022-07-18 18:59:01 +0900

    16.const and static

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
+const NUMBER_OF_MONTHS: u32 = 12; // ALL CAPITAL LETTERS -> Global
+static SEASONS: [&str; 4] = ["Spring", "Summer", "Fall", "Winter"];
+
 fn main() {
-    let name = "서태지"; // $str is very fast
-    let other_name = String::from("Adrian Fahrenheit Țepeș"); // String is a bit slower, but more function / Heap / owned type / 24 bytes
-
-    // str is a dynamically sized type
-    // str은 가변 길이 이기때문에 pointer로 접근해야한다 (포인터는 사이즈가 같음.)
-
-    let a = "This is the string text".to_string();
-
-    let my_name = "Billybrobby";
-    let my_country = "USA";
-    let my_home = "Korea";
-
-    let together = format!(
-        "I am {} and I come from {} but I live in {}.",
-        my_name, my_country, my_home
-    );
-
-    let my_string: String = "Try to make this a String".into(); // String::from()과는 다르게, into()는 변형 가능한 타입이 다양하므로 명시적으로 적어줘야한다.
+    //  Const
+    // Static - fixed memory location, immutable, global
 }
